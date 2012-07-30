51cf270155adea721d3869a7ca68ee7bc222b915
Michael Schleichardt <michael@schleichardt.net>
2012-07-30 23:05:25 +0200

    added pseudo test for ci

---
+++ Plugin/test/TestDummy.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class TestDummy {
+    @Test
+    public void testDummy() throws Exception {
+        assertThat(1).isEqualTo(1);
+    }
+}
