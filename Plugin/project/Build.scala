812ecc9622008c1b77c542d5d7434295195950aa
Michael Schleichardt <michael@schleichardt.net>
2012-07-31 07:36:54 +0200

    removed comments

--- Plugin/project/Build.scala
+++ Plugin/project/Build.scala
@@ -1,15 +1,11 @@
 import sbt._
 import Keys._
-import PlayProject._
 
 object MailerBuild extends Build {
 
   lazy val moduleVersion =  "0.3-SNAPSHOT"
   lazy val moduleOrganization = "info.schleichardt"
   lazy val moduleName = "Play-2-Mailplugin"
-  //lazy val moduleScalaVersion = "2.9.1"
-  //lazy val playVersion =  "2.0.2"
-  //lazy val typesafeRepo = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val publishingFolder = Path.userHome.absolutePath+"/Projekte/schleichardt.github.com/jvmrepo"
 
   val moduleDependencies = Seq(
@@ -20,7 +16,5 @@ object MailerBuild extends Build {
       organization := moduleOrganization,
       publishTo := Some(Resolver.file("file",  new File(publishingFolder))(Resolver.mavenStylePatterns)),
       publishMavenStyle := true
-      //resolvers += typesafeRepo,
-      //libraryDependencies += "play" %% "play" % playVersion,
     )
 }
