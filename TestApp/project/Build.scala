f82732ffe7abb369d927414afd01ad48b2f439b0
Michael Schleichardt <michael@schleichardt.net>
2012-08-01 07:45:30 +0200

    version increment

--- TestApp/project/Build.scala
+++ TestApp/project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "info.schleichardt" %% "play-2-mailplugin" % "0.4-SNAPSHOT",
+      "info.schleichardt" %% "play-2-mailplugin" % "0.5-SNAPSHOT",
       "com.icegreen" % "greenmail" % "1.3"
     )
 
