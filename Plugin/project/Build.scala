0e1f16710a8da4b9c056647ed89aa7992a943570
Michael Schleichardt <michael@schleichardt.net>
2013-01-30 00:33:52 +0100

    publish settings for oss.sonatype.org

--- Plugin/project/Build.scala
+++ Plugin/project/Build.scala
@@ -12,9 +12,39 @@ object MailerBuild extends Build {
        "org.apache.commons" % "commons-email" % "1.2"
   )
 
+  lazy val githubPath = "schleichardt/Play-2-Mailplugin"
+
   lazy val main = PlayProject(moduleName, moduleVersion, moduleDependencies).settings(
       organization := moduleOrganization,
-      publishTo := Some(Resolver.file("file",  new File(publishingFolder))(Resolver.mavenStylePatterns)),
-      publishMavenStyle := true
-    )
+      publishMavenStyle := true,
+      publishArtifact in Test := false,
+      publishTo <<= version { (v: String) =>
+        val nexus = "https://oss.sonatype.org/"
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      },
+      pomIncludeRepository := { _ => false },
+      pomExtra := (
+        <url>https://github.com/{githubPath}</url>
+          <licenses>
+            <license>
+              <name>Apache 2</name>
+              <url>http://www.apache.org/licenses/LICENSE-2.0</url>
+              <distribution>repo</distribution>
+            </license>
+          </licenses>
+          <scm>
+            <url>git@github.com:{githubPath}.git</url>
+            <connection>scm:git:git@github.com:{githubPath}.git</connection>
+          </scm>
+          <developers>
+            <developer>
+              <id>schleichardt</id>
+              <name>Michael Schleichardt</name>
+              <url>http://michael.schleichardt.info</url>
+            </developer>
+          </developers>)
+      )
 }
